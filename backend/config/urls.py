aa4dcd899bee2b57cc28cef6042ed8c4e66ef7a3
riadelimemmedov <riad.elimemmedov@mail.ru>
2023-08-14 18:35:14 +0400

    Adding media and static url root

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('book/',include('book.urls',namespace='book')),
     path('author/',include('author.urls',namespace='author'))
 ]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
